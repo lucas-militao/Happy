cb80937187568e43941bfc7346f86b80cc8fc54d
lumilitas <lucasmengineer@edu.unifor.br>
2021-06-10 10:46:26 -0300

    Criado errorHandler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import 'express-async-errors';
 import './database/connection';
+import errorHandler from './errors/handler';
 import routes from './routes';
+
+
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(3333);
