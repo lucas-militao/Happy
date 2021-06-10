588e818af4a0535b71c34d497d9b2fe5d0130e54
lumilitas <lucasmengineer@edu.unifor.br>
2021-06-10 11:32:02 -0300

    Adicionado o cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 import './database/connection';
@@ -8,6 +9,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
