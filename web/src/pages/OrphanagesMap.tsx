257321d4ed86499908a78d3489b7ac0529559657
lumilitas <lucasmengineer@edu.unifor.br>
2021-06-10 13:47:48 -0300

    Implementada a estilização do marcador do mapa

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,20 @@
+import Leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import React from "react";
-import { FiPlus } from "react-icons/fi";
-import { Map, TileLayer } from "react-leaflet";
+import { FiArrowRight, FiPlus } from "react-icons/fi";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Link } from "react-router-dom";
-
-import 'leaflet/dist/leaflet.css'
-
 import mapMarkerImg from "../images/map-marker.svg";
-
 import "../styles/pages/orphanages-map.css";
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+
 
 function OrphanagesMap() {
   return (
@@ -35,6 +42,19 @@ function OrphanagesMap() {
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+        <Marker
+          position={[-3.7997898,-38.4836679]}
+          icon={mapIcon}
+        >
+
+   			 <Popup className="map-popup" closeButton={false} minWidth={240} maxWidth={240}>
+            Lar das meninas
+            <Link to="">
+              <FiArrowRight size={20} color="#FFF"/>
+            </Link>
+         </Popup>
+        </Marker>
       </Map>
 
       <Link to="" className="create-orphanage">
