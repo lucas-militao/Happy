06623ffdbbea70737b863d0c13ee3ab1bcc8d885
lumilitas <lucasmengineer@edu.unifor.br>
2021-06-14 13:47:50 -0300

    Criada rota para exibição estática de imagem

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
+import path from 'path';
 import './database/connection';
 import errorHandler from './errors/handler';
 import routes from './routes';
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
 app.listen(3333);
