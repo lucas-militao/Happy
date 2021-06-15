859948f379f20e044f4d30a24aeb2776f4080368
lumilitas <lucasmengineer@edu.unifor.br>
2021-06-15 15:00:43 -0300

    Criado o fluxo de cadastro

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,6 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import Header from './components/Header';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
 import OrphanageDetails from './pages/OrphanageDetails';
 import OrphanagesMap from './pages/OrphanagesMap';
 
@@ -11,10 +14,36 @@ export default function Routes() {
   return(
     <NavigationContainer>
       <Navigator screenOptions={{
-        headerShown: false
+        headerShown: false,
+        cardStyle: {backgroundColor: '#f2f3f5'}
       }}>
-        <Screen name="OrphanagesMap" component={OrphanagesMap} />
-        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+        <Screen 
+          name="OrphanagesMap"
+          component={OrphanagesMap} />
+
+        <Screen 
+          name="OrphanageDetails" 
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato"/>
+          }} />
+
+        <Screen 
+          name="SelectMapPosition" 
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa"/>
+          }} />
+
+        <Screen 
+          name="OrphanageData" 
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados"/>
+          }} />
       </Navigator>
     </NavigationContainer>
   )
