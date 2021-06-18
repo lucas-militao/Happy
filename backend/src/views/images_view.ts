90619b922e51169d80ba1de5b269b932d98554af
lumilitas <lucasmengineer@edu.unifor.br>
2021-06-18 10:45:31 -0300

    Implementando a página OrphanageDetails

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -5,7 +5,7 @@ export default {
   render(image: Image) {
     return{
       id: image.id,
-      url: `http://localhost:3333/uploads/${image.path}`
+      url: `http://192.168.0.123:3333/uploads/${image.path}` //Lembrar de alterar para localhost quando for utilizar emulador
     };
   },
 
